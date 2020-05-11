7d37d3cd0520a57ae356b934b70cd4c22a8b17cb
Pat McClernan <patmccler@gmail.com>
2020-05-11 12:07:25 -0400

    builds jukebox and updates tests to have required output consistent 'now playing :'

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,93 @@
+require "pry"
+class Jukebox
+  attr_accessor :songs, :input
 
+  def initialize(songs)
+    @songs = songs
+  end
+
+  def get_input
+    puts "What do you want to do?"
+    @input = gets.chomp
+  end
+
+  def call
+    done = false
+
+    until done
+      puts "Welcome to the Jukebox! Type 'help' to see the list of available commands"
+      get_input
+
+      case @input
+      when "exit"
+        done = true
+      when "help" || "play"
+        do_input
+      else
+        puts invalid_input
+      end
+    end
+  end
+
+  def invalid_input
+    puts "Sorry, I didn't get that. Try again?"
+  end
+
+  def help
+    puts "Available commands:"
+    puts "help"
+    puts "list"
+    puts "play"
+    puts "exit"
+  end
+
+  def do_command
+    case @input
+    when "help"
+      help
+    when "play"
+      play
+    when "list"
+      list
+    when "exit"
+      exit_jukebox
+    end
+  end
+
+  def exit_jukebox
+    "Thanks for playing!"
+  end
+
+  def list
+    @songs.each_with_index { |song, index| puts "#{index + 1}. #{song}" }
+  end
+
+  def get_song_by_number(num)
+    return nil if num.to_i <= 0
+
+    @songs[num.to_i - 1]
+  end
+
+  def get_song_by_name(name)
+    @songs.find { |song| song == name }
+  end
+
+  def get_song_by_name_or_number(song)
+    get_song_by_name(song) || get_song_by_number(song)
+  end
+
+  def play
+    puts "Enter a song number or enter the artist and song title, as shown in the list above"
+    play_input = gets.chomp
+    song = get_song_by_name_or_number(play_input)
+
+    if play_input == "help"
+      puts "Enter a song number or enter the artist and song title, as shown in the list above"
+      puts "Enter 'Exit' to go back"
+    elsif song
+      puts "Now playing: #{get_song_by_name_or_number(song)}"
+    else
+      puts "sorry, please enter a valid song or type 'help' for more options."
+    end
+  end
+end
--- spec/01_jukebox_spec.rb
+++ spec/01_jukebox_spec.rb
@@ -1,5 +1,5 @@
-describe 'Jukebox' do
-  let(:songs) {
+describe "Jukebox" do
+  let(:songs) do
     [
       "Phoenix - 1901",
       "Tokyo Police Club - Wait Up",
@@ -9,11 +9,11 @@ describe 'Jukebox' do
       "The Cults - Abducted",
       "Phoenix - Consolation Prizes",
       "Harry Chapman - Cats in the Cradle",
-      "Amos Lee - Keep It Loose, Keep It Tight"
+      "Amos Lee - Keep It Loose, Keep It Tight",
     ]
-  }
+  end
 
-  let(:song_regexes) {
+  let(:song_regexes) do
     [
       /Phoenix - 1901/,
       /Tokyo Police Club - Wait Up/,
@@ -23,11 +23,11 @@ describe 'Jukebox' do
       /The Cults - Abducted/,
       /Phoenix - Consolation Prizes/,
       /Harry Chapman - Cats in the Cradle/,
-      /Amos Lee - Keep It Loose, Keep It Tight/
+      /Amos Lee - Keep It Loose, Keep It Tight/,
     ]
-  }
+  end
 
-  let(:song_regexes_with_indicies) {
+  let(:song_regexes_with_indicies) do
     [
       /1\. Phoenix - 1901/,
       /2\. Tokyo Police Club - Wait Up/,
@@ -37,57 +37,56 @@ describe 'Jukebox' do
       /6\. The Cults - Abducted/,
       /7\. Phoenix - Consolation Prizes/,
       /8\. Harry Chapman - Cats in the Cradle/,
-      /9\. Amos Lee - Keep It Loose, Keep It Tight/
+      /9\. Amos Lee - Keep It Loose, Keep It Tight/,
     ]
-  }
+  end
 
   let(:jukebox) { Jukebox.new(songs) }
 
-  describe '#new' do
-    it 'accepts a list of songs on initialization' do
-      expect{Jukebox.new(songs)}.to_not raise_error
+  describe "#new" do
+    it "accepts a list of songs on initialization" do
+      expect { Jukebox.new(songs) }.to_not raise_error
     end
 
-    it 'assigns the list of songs to an instance variable' do
+    it "assigns the list of songs to an instance variable" do
       expect(jukebox.instance_variable_get(:@songs)).to eq(songs)
     end
 
-    it 'creates new instances of itself' do
+    it "creates new instances of itself" do
       expect(jukebox).to be_an_instance_of(Jukebox)
     end
   end
 
-  describe '#input' do 
-    it "is a setter to get a user's input and is a getter to retrieve a user's input" do 
+  describe "#input" do
+    it "is a setter to get a user's input and is a getter to retrieve a user's input" do
       jukebox.input = "play"
       expect(jukebox.input).to eq("play")
     end
-
   end
 
-  describe '#call' do
-    it 'responds to a call method' do
+  describe "#call" do
+    it "responds to a call method" do
       expect(jukebox).to respond_to(:call)
     end
   end
 
-  describe '#help' do
-    it 'prints the available commands' do
+  describe "#help" do
+    it "prints the available commands" do
       help_output = capture_stdout { jukebox.help }
       expect(help_output).to include("help" && "list" && "play" && "exit")
     end
   end
 
-  describe '#get_input' do 
-    it 'prints a prompt for input to the user and stores the user response' do 
+  describe "#get_input" do
+    it "prints a prompt for input to the user and stores the user response" do
       jukebox.stub(:gets).and_return("help")
-      get_input_output = capture_stdout {jukebox.get_input}
+      get_input_output = capture_stdout { jukebox.get_input }
       expect(jukebox.input).to eq("help")
     end
   end
 
-  describe '#do_command' do 
-    it 'calls the get_input method and invokes the appropriate method depending on the input of the user' do 
+  describe "#do_command" do
+    it "calls the get_input method and invokes the appropriate method depending on the input of the user" do
       jukebox.stub(:gets).and_return("help")
       get_input_output = jukebox.get_input
       do_input_output = capture_stdout { jukebox.do_command }
@@ -95,8 +94,8 @@ describe 'Jukebox' do
     end
   end
 
-  describe '#list' do
-    it 'lists the songs a user can play' do
+  describe "#list" do
+    it "lists the songs a user can play" do
       list_output = capture_stdout { jukebox.list }
       song_regexes.each do |regex|
         expect(list_output).to match(regex)
@@ -104,20 +103,20 @@ describe 'Jukebox' do
     end
   end
 
-  describe '#play' do
-    it 'asks the user what song they\'d like to play' do
+  describe "#play" do
+    it "asks the user what song they'd like to play" do
       jukebox.stub(:gets).and_return("1")
       play_output = capture_stdout { jukebox.play }
       expect(play_output).to eq("Enter a song number or enter the artist and song title, as shown in the list above\nNow playing: Phoenix - 1901\n")
     end
-    
-    it 'accepts a song name and plays that song' do
+
+    it "accepts a song name and plays that song" do
       jukebox.stub(:gets).and_return("Phoenix - 1901")
       play_output = capture_stdout { jukebox.play }
-      expect(play_output).to match("Enter a song number or enter the artist and song title, as shown in the list above\nNow playing Phoenix - 1901\n")
+      expect(play_output).to match("Enter a song number or enter the artist and song title, as shown in the list above\nNow playing: Phoenix - 1901\n")
     end
 
-    it 'accepts a song number and plays that song' do 
+    it "accepts a song number and plays that song" do
       jukebox.stub(:gets).and_return("2")
       play_output = capture_stdout { jukebox.play }
       expect(play_output).to match("Enter a song number or enter the artist and song title, as shown in the list above\nNow playing: Tokyo Police Club - Wait Up\n")
